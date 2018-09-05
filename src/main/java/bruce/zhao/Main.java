874b88e035edf95ac12d3c6ff3149191783f031a
BruceZhaoR <brucezhaor2016@gmail.com>
2018-09-05 13:40:39 +0800

    print more resutl info

--- src/main/java/bruce/zhao/Main.java
+++ src/main/java/bruce/zhao/Main.java
@@ -7,6 +7,8 @@ import hex.genmodel.easy.exception.PredictException;
 import hex.genmodel.easy.prediction.BinomialModelPrediction;
 
 import java.io.IOException;
+import java.lang.reflect.Array;
+import java.util.Arrays;
 
 /**
  * @author zhaoweipc
@@ -19,24 +21,29 @@ public class Main {
                 MojoModel.load(ModelPath));
 
         RowData row = new RowData();
-        row.put("AGE", "68");
-        row.put("RACE", "2");
+        row.put("AGE", "71");
+        row.put("RACE", "1");
         row.put("DCAPS", "2");
         row.put("VOL", "0");
-        row.put("GLEASON", "6");
+        row.put("GLEASON", "8");
 
         BinomialModelPrediction pred = model.predictBinomial(row);
 
         // 结果打印
         System.out.println("Has penetrated the prostatic capsule (1=yes; 0=no): " + pred.label);
-        System.out.print("Class probabilities: ");
-        for (int i = 0; i < pred.classProbabilities.length; i++) {
-            if(i > 0) {
-                System.out.print(",");
-            }
-            System.out.print(pred.classProbabilities[i]);
-        }
-        System.out.println("");
+//        System.out.print("Class probabilities: ");
+//        for (int i = 0; i < pred.classProbabilities.length; i++) {
+//            if(i > 0) {
+//                System.out.print(",");
+//            }
+//            System.out.print(pred.classProbabilities[i]);
+//        }
+//        System.out.println("");
+
+        System.out.println("labelIndex: " + pred.labelIndex);
+        System.out.println("label: " + pred.label);
+        System.out.println("classProbability0: " + pred.classProbabilities[0]);
+        System.out.println("classProbability1: " + pred.classProbabilities[1]);
     }
 
 }
